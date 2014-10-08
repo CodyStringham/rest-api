cca0b061a63041d80435e0b721c8e7549b9f3ea6
codystringham <cs.negativ@gmail.com>
2014-10-08 14:19:15 -0600

    adds the api base controller

---
+++ app/controllers/api/base_controller.rb
@@ -0,0 +1,56 @@
+module Api
+  class BaseController < ApplicationController
+    protect_from_forgery with: :null_session
+    before_action :set_resource, only: [:destroy, :show, :update]
+    respond_to :json
+
+    private
+
+      # Returns the resource from the created instance variable
+      # @return [Object]
+      def get_resource
+        instance_variable_get("@#{resource_name}")
+      end
+
+      # Returns the allowed parameters for searching
+      # Override this method in each API controller
+      # to permit additional parameters to search on
+      # @return [Hash]
+      def query_params
+        {}
+      end
+
+      # Returns the allowed parameters for pagination
+      # @return [Hash]
+      def page_params
+        params.permit(:page, :page_size)
+      end
+
+      # The resource class based on the controller
+      # @return [Class]
+      def resource_class
+        @resource_class ||= resource_name.classify.constantize
+      end
+
+      # The singular name for the resource class based on the controller
+      # @return [String]
+      def resource_name
+        @resource_name ||= self.controller_name.singularize
+      end
+
+      # Only allow a trusted parameter "white list" through.
+      # If a single resource is loaded for #create or #update,
+      # then the controller for the resource must implement
+      # the method "#{resource_name}_params" to limit permitted
+      # parameters for the individual model.
+      def resource_params
+        @resource_params ||= self.send("#{resource_name}_params")
+      end
+
+      # Use callbacks to share common setup or constraints between actions.
+      def set_resource(resource = nil)
+        resource ||= resource_class.find(params[:id])
+        instance_variable_set("@#{resource_name}", resource)
+      end
+  end
+end
