aa6562d09dc1867434b1a388ff6eb0306a50dbee
codystringham <cs.negativ@gmail.com>
2014-10-08 15:36:09 -0600

    removes json default from routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   #resources :dreams, :users
 
   namespace :api do
-    resources :dreams, :users, defaults: {format: "json"}
+    resources :dreams, :users
   end
 
 
